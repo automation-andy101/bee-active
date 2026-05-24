e01b38678f9d4f44d18d7f2d86709c78631a415a
Andy <automation.andy101@gmail.com>
2026-05-24 23:01:44 +0100

    Initial Bee Active project setup

---
+++ backend-api/src/main/java/com/devinedevelopment/bee_active_api/BeeActiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.devinedevelopment.bee_active_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeActiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeActiveApiApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/main/java/com/devinedevelopment/bee_active_api/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.devinedevelopment.bee_active_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Bee Active API is running";
+    }
+}
